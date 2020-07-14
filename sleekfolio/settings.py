4aa0a8e13c8af4b4fe66fc515c200eb2a647659b
Oluwaseun Peter <adepeter26@gmail.com>
2020-07-14 16:51:59 +0100

    Commit for heroku deployment

--- sleekfolio/settings.py
+++ sleekfolio/settings.py
@@ -147,4 +147,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Email Backend
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
+
